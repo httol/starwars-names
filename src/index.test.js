9bba02e6f91d740dc7809d9842375dd449268585
httol <13821750403@invn.com.cn>
2020-09-21 18:05:03 +0800

    chore: add travis config, conventional-change-log, and sematic-release
    fix #1

---
+++ src/index.test.js
@@ -0,0 +1,27 @@
+var expect = require('chai').expect;
+var starWars = require('./index.js');
+
+describe('starwars name', function () {
+  describe('all', function () {
+    it('should be an array of strings', function () {
+      expect(starWars.all).to.satisfy(isArrayOfStrings);
+
+      function isArrayOfStrings(array) {
+        return array.every(function (item) {
+          return typeof item === 'string';
+        });
+      }
+    });
+
+    it('shoule contains Blark O.Bama',function(){
+        expect(starWars.all).to.include('Blark O.Bama');
+    });
+  });
+
+  describe('random',function(){
+      it('should return a random item from the starwars names',function(){
+          var randomItem = starWars.random();
+          expect(starWars.all).to.include(randomItem);
+      })
+  })
+});
