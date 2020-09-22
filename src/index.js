773682af12f4294a9171274725caa5b967fb504a
httol <13821750403@invn.com.cn>
2020-09-22 14:19:03 +0800

    chore(random): add ablity to get random item if pass a number
    BREAKING CHANGE: closes
    
    fix #1

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 var uniqureRandomArray = require('unique-random-array');
 var starWarsNames = require('./star-wars-names.json');
+var getRandomItem = uniqureRandomArray(starWarsNames);
 
 module.exports = {
   all: starWarsNames,
-  random: uniqureRandomArray(starWarsNames),
+  random: random,
 };
+
+function random(number) {
+  if (number === undefined) {
+    return getRandomItem();
+  } else {
+    var randomitems = [];
+    for (let index = 0; index < number; index++) {
+      randomitems.push(getRandomItem());
+    }
+    return randomitems;
+  }
+}
--- src/index.test.js
+++ src/index.test.js
@@ -14,15 +14,23 @@ describe('starwars name', function () {
       }
     });
 
-    it('shoule contains Blark O.Bama',function(){
-        expect(starWars.all).to.include('Blark O.Bama');
+    it('shoule contains Blark O.Bama', function () {
+      expect(starWars.all).to.include('Blark O.Bama');
     });
   });
 
-  describe('random',function(){
-      it('should return a random item from the starwars names',function(){
-          var randomItem = starWars.random();
-          expect(starWars.all).to.include(randomItem);
-      })
-  })
+  describe('random', function () {
+    it('should return a random item from the starwars names', function () {
+      var randomItem = starWars.random();
+      expect(starWars.all).to.include(randomItem);
+    });
+
+    it('should return an array of random item if pass number', function () {
+      var randomItems = starWars.random(3);
+      expect(randomItems).to.have.length(3);
+      randomItems.forEach((item) => {
+        expect(starWars.all).to.include(item);
+      });
+    });
+  });
 });
