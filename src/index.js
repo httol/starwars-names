e944b7fa1dbaefa0c0680ea6a57e0f1833b6be8a
httol <13821750403@invn.com.cn>
2020-12-14 17:20:21 +0800

    feat: semantic

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import uniqureRandomArray from 'unique-random-array';
 import starWarsNames from './star-wars-names.json';
 var getRandomItem = uniqureRandomArray(starWarsNames);
-// new version
+// semantic
 module.exports = {
   all: starWarsNames,
   random: random,
